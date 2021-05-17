371d4c29ca2e47d101ba66251cc025a8452454cb
mikan3rd <mikan.no.kikoushi@gmail.com>
2021-05-18 00:37:27 +0900

    chore: backend main.ts

--- packages/backend/src/main.ts
+++ packages/backend/src/main.ts
@@ -1,9 +1,58 @@
 import { NestFactory } from "@nestjs/core";
+import * as Sentry from "@sentry/node";
+import { locale } from "dayjs";
+import { Request } from "express";
+import admin from "firebase-admin";
+import morgan from "morgan";
 
-import { AppModule } from "./app.module";
+import "dayjs/locale/ja";
+
+import { SentryInterceptor } from "@/interceptors/sentry.interceptor";
+import { AppModule } from "@/modules/app.module";
+
+locale("ja");
+
+admin.initializeApp({
+  credential: admin.credential.cert({
+    projectId: process.env.FIREBASE_PROJECT_ID,
+    clientEmail: process.env.FIREBASE_CLIENT_EMAIL,
+    // https://stackoverflow.com/a/41044630/1332513
+    privateKey: process.env.FIREBASE_PRIVATE_KEY?.replace(/\\n/g, "\n"),
+  }),
+});
 
 async function bootstrap() {
-  const app = await NestFactory.create(AppModule);
-  await app.listen(3000);
+  const app = await NestFactory.create(AppModule, {
+    logger: ["debug", "warn", "error"],
+  });
+
+  Sentry.init({
+    dsn: process.env.SENTRY_DSN,
+    environment: process.env.SENTRY_ENV,
+    tracesSampleRate: 1.0,
+  });
+  app.useGlobalInterceptors(new SentryInterceptor());
+
+  morgan.token("graphql-query", (req: Request) => {
+    const { body } = req;
+
+    if (!body) {
+      return;
+    }
+
+    const { operationName, variables } = body;
+    if (operationName) {
+      return `\nOperation Name: ${operationName}\nVariables: ${JSON.stringify(variables)}`;
+    }
+    return;
+  });
+  app.use(morgan(":method :url :status :response-time ms :graphql-query"));
+
+  app.enableCors();
+  const server = await app.listen(process.env.PORT || 3300);
+  if (process.env.HTTPS_TIMEOUT) {
+    server.setTimeout(1000 * 60 * Number(process.env.HTTPS_TIMEOUT));
+  }
 }
+
 bootstrap();
